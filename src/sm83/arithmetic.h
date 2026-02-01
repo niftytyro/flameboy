91814d981fdb8a736705c447e98ad2f0a0fdcdca
Udasi Tharani <udasitharani@gmail.com>
2026-02-01 23:02:13 +0530

    add new arithmetic instructions to header

--- src/sm83/arithmetic.h
+++ src/sm83/arithmetic.h
@@ -13,4 +13,25 @@ void add_HL_r16(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_by
 void add_HL_SP(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
 void add_SP_e8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
 
+void cp_A_r8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void cp_A_HLa(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void cp_A_n8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+
+void dec_r8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void dec_HLa(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void dec_r16(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void dec_SP(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+
+void inc_r8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void inc_HLa(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void inc_r16(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void inc_SP(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+
+void sbc_A_r8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void sbc_A_HLa(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void sbc_A_n8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void sub_A_r8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void sub_A_HLa(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+void sub_A_n8(uint8_t *instruction, uint8_t *cpu_cycles, uint8_t *number_of_bytes);
+
 #endif
