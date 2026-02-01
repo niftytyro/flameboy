55f18f8e4a62f58c1e78fa923303d29ac2a315fb
Udasi Tharani <udasitharani@gmail.com>
2026-02-01 18:22:41 +0530

    implement load module separately

---
+++ src/sm83/load.c
@@ -0,0 +1,285 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "../addressing/addressing.h"
+#include "registers.h"
+
+const int BASE_REGISTER_INDEX = 2;
+
+int extract_register_index(int low) {
+  // TODO implement full register index
+  int i = low % 0x8;
+  i += BASE_REGISTER_INDEX;
+  i %= 0x8;
+
+  return i;
+}
+
+int extract_half_register_index(int low) {
+  int i = low % 0x8;
+  i += BASE_REGISTER_INDEX;
+  i %= 0x8;
+
+  return i;
+}
+
+void ld_R8_R8(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+
+  int register_index = extract_half_register_index(low),
+      target_register_index = BASE_REGISTER_INDEX + (*instruction % 0x40) / 0x8;
+
+  if (target_register_index != 0x6 && register_index != 0x6) {
+    write_half_register(target_register_index,
+                        read_half_register(register_index));
+  }
+
+  *cpu_cycles = 1;
+  *number_of_bytes = 1;
+}
+
+void ld_R8_N8(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+
+  int register_index = extract_half_register_index(low);
+  uint8_t operand = *(instruction + 1);
+
+  if (register_index != 0x6) {
+    write_half_register(register_index, operand);
+  }
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 2;
+}
+
+void ld_R16_N16(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+
+  int register_index = extract_half_register_index(low);
+  uint8_t byte1 = *(instruction + 1), byte2 = *(instruction + 2);
+
+  if (register_index != 0x6) {
+    write_register(register_index, byte1, byte2);
+  }
+
+  *cpu_cycles = 3;
+  *number_of_bytes = 3;
+}
+
+void ld_HLa_r8(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+  int register_index = extract_half_register_index(low);
+
+  uint16_t address = read_register_by_name("HL");
+  uint8_t byte = read_half_register(register_index);
+
+  write_address(address, byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_HLa_n8(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t byte = *(instruction + 1);
+
+  write_address(address, byte);
+
+  *cpu_cycles = 3;
+  *number_of_bytes = 2;
+}
+
+void ld_r8_HLa(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+  int register_index = extract_half_register_index(low);
+
+  uint16_t address = read_register_by_name("HL");
+  uint8_t *byte = read_address(address);
+
+  write_half_register(register_index, *byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_r16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+  int register_index = extract_register_index(low);
+
+  uint16_t address = read_register(register_index);
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_n16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  uint16_t address = *(instruction + 2) * 0x100 + *(instruction + 1);
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  *cpu_cycles = 4;
+  *number_of_bytes = 3;
+}
+
+void ldh_n16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  uint16_t address = 0xff00 + *(instruction + 1);
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  *cpu_cycles = 3;
+  *number_of_bytes = 2;
+}
+
+void ldh_Ca_A(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  uint16_t address = 0xff00 + read_half_register_by_name('C');
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_A_r16(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  int low = *instruction & 0x1;
+  int register_index = extract_register_index(low);
+
+  uint16_t address = read_register(register_index);
+  uint8_t *byte = read_address(address);
+
+  write_half_register_by_name('A', *byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_A_n16(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  uint16_t address = *(instruction + 2) * 0x100 + *(instruction + 1);
+  uint8_t *byte = read_address(address);
+
+  write_half_register_by_name('A', *byte);
+
+  *cpu_cycles = 4;
+  *number_of_bytes = 3;
+}
+
+void ldh_A_Ca(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  uint16_t address = 0xff00 + read_half_register_by_name('C');
+  uint8_t *byte = read_address(address);
+
+  write_half_register_by_name('A', *byte);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ldh_HLIa_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  increment_register_by_name("HL");
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ldh_HLDa_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t byte = read_half_register_by_name('A');
+
+  write_address(address, byte);
+
+  decrement_register_by_name("HL");
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ldh_A_HLIa(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t *byte = read_address(address);
+
+  write_half_register('A', *byte);
+
+  increment_register_by_name("HL");
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ldh_A_HLDa(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t *byte = read_address(address);
+
+  write_half_register('A', *byte);
+
+  decrement_register_by_name("HL");
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
+
+void ld_SP_n16(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes) {
+  uint16_t address = read_register_by_name("HL");
+  uint8_t byte1 = *(instruction + 1), byte2 = *(instruction + 2);
+
+  write_register_by_name("SP", byte1, byte2);
+
+  *cpu_cycles = 3;
+  *number_of_bytes = 3;
+}
+
+void ld_HL_SPe8(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes) {
+  char e8 = *(instruction + 1);
+  uint16_t value = read_register_by_name("SP");
+  uint16_t new_value = value + e8;
+
+  write_register_by_name("SP", value & 0xff00, value & 0xff00);
+
+  write_register_by_name("HL", value & 0xff00, value & 0xff00);
+
+  if (e8 < 0) {
+    write_flags(0, 0, (new_value & 0xf) > (value & 0xf), new_value > value);
+  } else {
+    write_flags(0, 0, (new_value & 0xf) < (value & 0xf), new_value < value);
+  }
+
+  *cpu_cycles = 3;
+  *number_of_bytes = 2;
+}
+
+void ld_SP_HL(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes) {
+  uint16_t value = read_register_by_name("HL");
+
+  write_register_by_name("SP", value & 0xff00, value & 0xff00);
+
+  *cpu_cycles = 2;
+  *number_of_bytes = 1;
+}
---
+++ src/sm83/load.h
@@ -0,0 +1,66 @@
+#ifndef CPU_LOAD_INS
+#define CPU_LOAD_INS
+
+#include <stdint.h>
+
+void ld_R8_R8(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ld_R8_N8(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ld_R16_N16(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ld_HLa_r8(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ld_HLa_n8(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ld_r8_HLa(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ld_r16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ld_n16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ldh_n16a_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ldh_Ca_A(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ld_A_r16(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ld_A_n16(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ldh_A_Ca(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+void ldh_HLIa_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ldh_HLDa_A(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ldh_A_HLIa(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ldh_A_HLDa(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ld_SP_n16(uint8_t *instruction, uint8_t *cpu_cycles,
+               uint8_t *number_of_bytes);
+
+void ld_HL_SPe8(uint8_t *instruction, uint8_t *cpu_cycles,
+                uint8_t *number_of_bytes);
+
+void ld_SP_HL(uint8_t *instruction, uint8_t *cpu_cycles,
+              uint8_t *number_of_bytes);
+
+#endif
